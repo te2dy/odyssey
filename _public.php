edc8cb8c0983142fe506464cb67bc60916ed92df
te2dy <teddyenligne@pm.me>
2022-08-14 14:23:36 +0200

    Essai correction erreur URL relative

--- _public.php
+++ _public.php
@@ -158,7 +158,7 @@ class tplOrigineMiniTheme
      */
     public static function origineMiniURIRelative()
     {
-        return $_SERVER['REQUEST_URI'];
+        return '<?php echo $_SERVER['REQUEST_URI']; ?>';
     }
 
     /**
