0a9cdc15802a8d69b25df23788b5555f6b8082d8
te2dy <teddyenligne@pm.me>
2022-10-28 19:41:58 +0200

    Bêta 17

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Origine Mini',                  // Name
     'A minimalistic Dotclear theme', // Description
     'Teddy',                         // Author
-    '0.1.0.16',                      // Version
+    '0.1.0.17',                      // Version
     [
         'type'                 => 'theme',
         'widgettitleformat'    => '<h3>%s</h3>', // h3 instead of h2 by default. No class needed.
