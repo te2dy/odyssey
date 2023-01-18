27bfe4c1a2e181bc69445da334c567c6dc6d3b45
te2dy <teddyenligne@pm.me>
2023-01-18 11:45:08 +0100

    Origine Mini 1.3.1-beta

--- _define.php
+++ _define.php
@@ -2,8 +2,9 @@
 /**
  * Origine Mini, a minimal theme for Dotclear.
  *
- * @author Teddy
- * @copyright GPL-3.0
+ * @author    Teddy <zozxebpyr@mozmail.com>
+ * @copyright 2022-2023 Teddy
+ * @license   GPL-3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
  */
 
 if (!defined('DC_RC_PATH')) {
@@ -14,12 +15,16 @@ $this->registerModule(
     'Origine Mini',             // Name
     'A minimal Dotclear theme', // Description
     'Teddy',                    // Author
-    '1.3',                      // Version
+    '1.3.1-beta',               // Version
     [
         'requires'             => [['core', '2.24']],
-        'standalone_config'    => true, // Allows a full control for the theme configurator.
         'type'                 => 'theme',
-        'widgettitleformat'    => '<h3>%s</h3>', // h3 instead of h2 by default. No class needed.
+
+        // Allows a full control for the theme configurator.
+        'standalone_config'    => true,
+
+        // Replaces default heading levels.
+        'widgettitleformat'    => '<h3>%s</h3>',
         'widgetsubtitleformat' => '<h4>%s</h4>'
     ]
 );
