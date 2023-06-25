046605b87d185af721fdf28be06ff3037b7cbf6d
te2dy <teddyenligne@pm.me>
2023-06-25 23:28:45 +0200

    Création fichier My.php

---
+++ src/My.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Origine Mini, a minimal theme for Dotclear.
+ *
+ * @author    Teddy <zozxebpyr@mozmail.com>
+ * @copyright 2022-2023 Teddy
+ * @license   GPL-3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
+ */
+
+namespace Dotclear\Theme\originemini;
+
+use Dotclear\Module\MyTheme;
+
+class My extends MyTheme
+{
+}
