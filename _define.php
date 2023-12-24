c23dd0cd53b0107645767f35b3c86e9cb5f7aa99
te2dy <teddyenligne@pm.me>
2023-12-24 14:15:33 +0100

    Odyssey 0.4-alpha

--- _define.php
+++ _define.php
@@ -11,7 +11,7 @@ $this->registerModule(
     'Odyssey',
     'Write your own with this theme',
     'Teddy',
-    '0.3.1-alpha',
+    '0.4-alpha',
     [
         'requires' => [
             ['core', '2.28'],
