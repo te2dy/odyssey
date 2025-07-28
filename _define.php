d68e390d241be70cebf88a381bba8733b96731e3
te2dy <52241628+te2dy@users.noreply.github.com>
2025-07-28 16:40:42 +0200

    Odyssée 2.1.3

--- _define.php
+++ _define.php
@@ -11,7 +11,7 @@ $this->registerModule(
     'Odyssey',
     'A simple and customizable theme to write your own odyssey',
     'Teddy',
-    '2.1.2',
+    '2.1.3',
     [
         'requires' => [
             ['core', '2.30'],
