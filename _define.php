34b42ce5eccedfe532656f2e040d4074ed8f618f
te2dy <52241628+te2dy@users.noreply.github.com>
2025-12-31 16:57:37 +0100

    Odyssée 2.22

--- _define.php
+++ _define.php
@@ -3,7 +3,7 @@
  * Odyssey, a simple and customizable Dotclear theme.
  *
  * @author    Teddy <contact.teddy@laposte.net>
- * @copyright 2022-2025 Teddy
+ * @copyright 2022-2026 Teddy
  * @license   GPL-3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
  */
 
@@ -11,17 +11,16 @@ $this->registerModule(
     'Odyssey',
     'A simple and customizable theme to write your own odyssey',
     'Teddy',
-    '2.21',
+    '2.22',
     [
         'requires' => [
-            ['core', '2.36'],
-            ['php', '8.1']
+            ['core', '2.36']
         ],
 
         'type' => 'theme',
 
         // 'support'    => 'https://forum.dotclear.org/viewtopic.php?id=51635', // The forum is offline.
-        'details'    => 'https://themes.dotaddict.org/galerie-dc2/details/odyssey',
+        'details'    => 'https://dotclear.org/theme/detail/Odyssey',
         'repository' => 'https://raw.githubusercontent.com/te2dy/odyssey/main/dcstore.xml',
 
         // Allows a full control for the theme configurator.
